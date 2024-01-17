54f68378390d13eb88fa58a8d5c23f5d805841ea
ApA gPj <peymanjahangiri@gmail.com>
2024-01-18 01:04:38 +0330

    Image Slider Component Added

--- flowerist/src/App.jsx
+++ flowerist/src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import './App.css'
-import Header from './components/header/Header'
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import ImageSlider from "./components/imageSlider/ImageSlider";
+import sliderImg1 from "./assets/hero1.jpg";
+import sliderImg2 from "./assets/hero2.jpg";
 
 function App() {
+  const imageUrls = [sliderImg1, sliderImg2];
 
   return (
     <>
-    <Header />
+      <Header />
+      <ImageSlider images={imageUrls} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- flowerist/src/components/imageSlider/ImageSlider.jsx
+++ flowerist/src/components/imageSlider/ImageSlider.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styles from "./ImageSlider.module.css";
+import sliderImg1 from "../../assets/hero1.jpg";
+import sliderImg2 from "../../assets/hero2.jpg";
 
 const ImageSlider = () => {
-  return (
-    <div>ImageSlider</div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default ImageSlider
+  const images = [sliderImg1, sliderImg2];
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : images.length - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex < images.length - 1 ? prevIndex + 1 : 0
+    );
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleNext();
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  }, [currentIndex]);
+
+  return (
+    <div className={styles.imageSlider}>
+      {/* image container */}
+      <div>
+        <img
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          style={{ width: "100%" }}
+        />
+      </div>
+
+      {/* text container */}
+      <div className={styles.textContainer}>
+        <p className={styles.textHead}>گل‌های تازه، هدیه‌هایی جذاب</p>
+        <h2 className={styles.textTitle}>گل‌های زیبا را بخشی از زندگی خود کنید</h2>
+        <button className={styles.textBtn}>خرید</button>
+      </div>
+
+      {/* button container */}
+      <div className={styles.buttonContainer}>
+        <button
+          onClick={handlePrev}
+          className={`${styles.btn} ${
+            currentIndex === 1 ? `${styles.active}` : ""
+          }`}
+        ></button>
+        <button
+          onClick={handleNext}
+          className={`${styles.btn}  ${
+            currentIndex === 0 ? `${styles.active}` : ""
+          }`}
+        ></button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
