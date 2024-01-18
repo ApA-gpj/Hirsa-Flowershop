7f873998d93695f20444d61ba981655b41bb09c0
ApA gPj <peymanjahangiri@gmail.com>
2024-01-18 17:32:28 +0330

    init products component

--- flowerist/src/App.jsx
+++ flowerist/src/App.jsx
@@ -6,6 +6,8 @@ import sliderImg1 from "./assets/hero1.jpg";
 import sliderImg2 from "./assets/hero2.jpg";
 import Benefits from "./components/benefits/Benefits";
 import Categories from "./components/categories/Categories";
+import Products from "./components/products/Products";
+
 function App() {
   const imageUrls = [sliderImg1, sliderImg2];
 
@@ -14,7 +16,8 @@ function App() {
       <Header />
       <ImageSlider images={imageUrls} />
       <Benefits />
-      <Categories/>
+      <Categories />
+      <Products />
     </>
   );
 }
