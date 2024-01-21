6ca29bce00b9d967c30c74795c0b14f88d32e340
ApA gPj <86545970+ApA-gpj@users.noreply.github.com>
2024-01-21 22:06:35 +0330

    Customize component icons added

--- src/components/customize/Customize.jsx
+++ src/components/customize/Customize.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from "./Customize.module.css";
 import BasicModal from "../modal/BasicModal";
-import { FaInstagram } from "react-icons/fa";
-import { LuMailPlus } from "react-icons/lu";
-import { MdAddCall } from "react-icons/md";
+import { FaInstagram, FaTelegram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import { IoCall, IoMailOutline } from 'react-icons/io5';
 
 const Customize = () => {
   return (
@@ -27,19 +26,12 @@ const Customize = () => {
             modalHeadText={"لطفا یکی از گزینه‌های زیر را انتخاب کنید ⬇ "}
             modalBodyText={
               <div className={styles.iconsContainer}>
-                <a
-                  href="https://instagram.com/HirsaFlowerShop"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <FaInstagram className={styles.callicon} />
-                </a>
-                <a href="mailto:HirsaFlowershop@gmail.com">
-                  <LuMailPlus className={styles.callicon} />
-                </a>
-                <a href="tel:+989302209897">
-                  <MdAddCall className={styles.callicon} />
-                </a>
+                <a className={styles.socialMediaLink} rel='noopener noreffer' target='_blank' href='https://instagram.com/hirsaflowershop'> اینستاگرام <FaInstagram className={styles.callicon} /></a>
+                <a className={styles.socialMediaLink} rel='noopener noreffer' target='_blank' href='https://telegram.me/hirsaflowershop'> تلگرام <FaTelegram className={styles.callicon} /></a>
+                <a className={styles.socialMediaLink} rel='noopener noreffer' target='_blank' href='https://twitter.com/hirsaflowershop'> توییتر <FaTwitter className={styles.callicon} /></a>
+                <a className={styles.socialMediaLink} rel='noopener noreffer' target='_blank' href='https://wa.me/+989999999999'> واتس‌‌اپ <FaWhatsapp className={styles.callicon} /></a>
+                <a className={styles.socialMediaLink} href="mailto:hirsaflowershop@gmail.com">ایمیل <IoMailOutline className={styles.callicon} /></a>
+                <a className={styles.socialMediaLink} href="tel:+989999999999">تماس <IoCall className={styles.callicon} /> </a>
               </div>
             }
           />
